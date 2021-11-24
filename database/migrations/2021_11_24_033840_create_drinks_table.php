a1f779420983f895b1af8487a3eebb634f7a5b7c
D1084181036 <d1084181036@gm.lhu.edu.tw>
2021-11-24 15:38:04 +0800

    表單建立

---
+++ database/migrations/2021_11_24_033840_create_drinks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDrinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drinks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100)->comment('飲料');
+            $table->tinyInteger('bid')->comment('品牌編號');
+            $table->integer('milliliters')->unsigned()->comment('毫升')->nullable();
+            $table->integer('price')->unsigned()->comment('價錢')->nullable();
+            $table->double('calories')->unsigned()->comment('熱量')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drinks');
+    }
+}
